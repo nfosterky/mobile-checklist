c78c8a9d959d44eb3527d32e1127bb74efc52e5c
Nathaniel Foster <nathanielsmusic@gmail.com>
2018-04-14 11:37:37 -0600

    Move styles to seperate file

---
+++ src/styles.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44
+  },
+});
