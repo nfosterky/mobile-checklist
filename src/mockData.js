78e4d6ac85516aba741fd3f1bb79bfc272407406
Nathaniel Foster <nathanielsmusic@gmail.com>
2018-04-14 11:27:58 -0600

    Add mock data, initial data structure

---
+++ src/mockData.js
@@ -0,0 +1,33 @@
+export default {
+  main: 564,
+  checklists: {
+    564: [
+      {
+        id: 98,
+        title: 'Do the laundry',
+        done: false,
+        sublist: 573
+      },
+      {
+        id: 99,
+        title: 'Clean room',
+        done: true,
+        sublist: null
+      }
+    ],
+    573: [
+      {
+        id: 98,
+        title: 'Wash laundry',
+        done: true,
+        sublist: 573
+      },
+      {
+        id: 99,
+        title: 'Dry laundry',
+        done: false,
+        sublist: null
+      }
+    ]
+  }
+}
