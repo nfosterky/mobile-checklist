fc3e30bb22d839f9528f062ca1c8088432ef2e91
Nathaniel Foster <nathanielsmusic@gmail.com>
2018-04-14 11:28:34 -0600

    Add flatlist for check items, add addItem button

--- src/MainNavigation.js
+++ src/MainNavigation.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
+import mockData from './mockData';
+
 class HomeScreen extends React.Component {
+  constructor() {
+    super();
+
+    this.addItem = this.addItem.bind(this);
+    this.navigate = this.navigate.bind(this);
+  }
+
+  addItem() {
+    console.warn('add item');
+  }
+
   navigate() {
     this.props.navigation.navigate('Home')
   }
@@ -10,7 +23,18 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text onPress={() => this.navigate()}>Home Screen</Text>
+        <FlatList
+          data={mockData.checklists[mockData.main]}
+          renderItem={ ({item}) => (
+            <Text 
+              style={styles.item} 
+              onPress={ () => this.navigateToAnswer(item) } >
+              {item.title}
+            </Text>
+          )
+          }
+        />
+        <Button onPress={this.addItem} title="Add Item" />
       </View>
     );
   }
@@ -19,5 +43,29 @@ class HomeScreen extends React.Component {
 export default StackNavigator({
   Home: {
     screen: HomeScreen,
+  },
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44
   },
 });
