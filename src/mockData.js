c657cfe9d11504106ce47d0962b203265235c681
Nathaniel Foster <nathanielsmusic@gmail.com>
2018-04-26 06:20:55 -0600

    Flatten data structure

--- src/mockData.js
+++ src/mockData.js
@@ -1,33 +1,37 @@
 export default {
   main: 564,
   checklists: {
-    564: [
-      {
-        id: 98,
-        title: 'Do the laundry',
-        done: false,
-        sublist: 573
-      },
-      {
-        id: 99,
-        title: 'Clean room',
-        done: true,
-        sublist: null
-      }
-    ],
-    573: [
-      {
-        id: 103,
-        title: 'Wash laundry',
-        done: true,
-        sublist: null
-      },
-      {
-        id: 992,
-        title: 'Dry laundry',
-        done: false,
-        sublist: null
-      }
-    ]
+    564: [ 98, 99 ],
+    573: [ 103, 992 ]
+  },
+  items: {
+    98: {
+      key: 98,
+      title: 'Do the laundry',
+      done: false,
+      sublist: 573,
+      parentListId: 564
+    },
+    99: {
+      key: 99,
+      title: 'Clean room',
+      done: true,
+      sublist: null,
+      parentListId: 564
+    },
+    103: {
+      key: 103,
+      title: 'Wash laundry',
+      done: true,
+      sublist: null,
+      parentListId: 573
+    },
+    992: {
+      key: 992,
+      title: 'Dry laundry',
+      done: false,
+      sublist: null,
+      parentListId: 573
+    }
   }
 }
